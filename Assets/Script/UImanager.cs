4956c913a765764f88ce139f02b7ba0a82820166
AestyMoee <swa.vincent@gmail.com>
2015-01-24 11:44:42 +0100

    UiPause

---
+++ Assets/Script/UImanager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UImanager : MonoBehaviour {
+
+	void Update(){
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			//mettre en pause
+			Debug.Log("pause");
+				}
+			
+
+	}
+
+	public void exitApplication() {
+		Application.Quit ();
+	}
+}

a19fff4cac97759e24a3ffc7b305b0b79311c355
Lyatus (TaxiZord) <lutopia@hotmail.fr>
2015-01-24 12:53:12 +0100

    Button mapping starts correct then fades to chaos

--- Assets/Script/ButtonManager.cs
+++ Assets/Script/ButtonManager.cs
@@ -5,26 +5,40 @@ public class ButtonManager : MonoBehaviour {
 	public GameObject buttonPrefab;
 	public float speed;
 	public float zoneDistance;
+	public float correctMappingModifier;
+	public float correctMappingProb;
 	
+	void Start(){
+		StartCoroutine(correctMappingCoroutine());
+	}
+	IEnumerator correctMappingCoroutine(){
+		while(true){
+			correctMappingProb *= correctMappingModifier;
+			Debug.Log (correctMappingProb);
+			yield return new WaitForSeconds(1f);
+		}
+	}
 	public void createButton(Vector3 position, string buttonName, Vector3 speed){
 		GameObject button = Instantiate(buttonPrefab) as GameObject;
 		button.GetComponent<Button>().init(buttonName,speed);
 		button.transform.parent = transform;
 		button.transform.localPosition = position;
 	}
-	public void createButtonForPlayer1(string buttonName, float delay){
-		createButton(new Vector3(-(zoneDistance+delay*speed),.5f,0),buttonName,new Vector3(speed,0,0));
-	}
-	public void createButtonForPlayer2(string buttonName, float delay){
-		createButton(new Vector3(zoneDistance+delay*speed,-.5f,0),buttonName,new Vector3(-speed,0,0));
-	}
-	public string randomButton(){
-		return Random.Range(1,3)+"_"+Random.Range(1,10);
-	}
 	public void beat(float delay){
-		string buttonName = randomButton();
-		if(Random.Range(0,2)==0)
-			createButtonForPlayer1(buttonName,delay);
-		else createButtonForPlayer2(buttonName,delay);
+		Vector3 position = new Vector3(zoneDistance+delay*this.speed,-.5f,0);
+		Vector3 speed = new Vector3(-this.speed,0,0);
+		string buttonName;
+		
+		if(Random.Range(0,2)==0){
+			position = -position;
+			speed = -speed;
+			buttonName = "1_"+Random.Range(1,10);
+		}
+		else buttonName = "2_"+Random.Range(1,10);
+		
+		if(Random.Range(0f,1f)>correctMappingProb)
+			buttonName = Random.Range(1,3)+"_"+Random.Range(1,10); // Use incorrect mapping because fuck them
+		
+		createButton(position,buttonName,speed);
 	}
 }

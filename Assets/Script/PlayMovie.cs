fc81fe79a466568fbd67da1ef1b928be03b9b683
Lyatus (TaxiZord) <lutopia@hotmail.fr>
2015-01-24 12:25:55 +0100

    Intro
    Intro now switches to Main when video is over or when any button is
    pressed.

--- Assets/Script/PlayMovie.cs
+++ Assets/Script/PlayMovie.cs
@@ -5,11 +5,16 @@ using System.Collections;
 [RequireComponent(typeof(AudioSource))]
 public class PlayMovie : MonoBehaviour {
 	public MovieTexture movie;
+	public string nextScene;
 	
 	void Start () {
 		GetComponent<Renderer>().material.mainTexture = movie;
 		GetComponent<AudioSource>().clip = movie.audioClip;
 		movie.Play();
 		GetComponent<AudioSource>().Play();
+	}
+	void Update(){
+		if(!movie.isPlaying || Input.anyKeyDown)
+			Application.LoadLevel(nextScene);
 	}
 }
